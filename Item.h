91e30e0ff3193b9d0982ab6ee4507dedb24880ee
ShawnYcx <scy12a@acu.edu>
2014-11-17 01:13:39 -0600

    Nth

--- Item.h
+++ Item.h
@@ -11,7 +11,8 @@ class Item{
 		Item(string thing, string info){getName(thing);}
 		virtual ~Item(){};
 
-		string getName(){return itemName;}
-		void getName(string thing){itemName = thing;}
+		string getName(){return itemName;} void getName(string thing){itemName = thing;}
+		string getDescript(){return itemInfo;} void getDescript(string info){itemInfo = info;}
 		virtual bool itemCondition(){return 0;}
 };
+
