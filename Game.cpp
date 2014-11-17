89a84d4d7c514fa9db4162b4a296c73e8db2ef9e
Mister2509 <ixn13b@acu.edu>
2014-11-16 19:41:58 -0600

    Update cpp file

---
+++ Game.cpp
@@ -0,0 +1,30 @@
+#include "Header.h"
+
+int main()
+{
+	Room* VillageCentLocs[4] = {LargeHouse = "North",Graveyard = "East",Shack = "South",Bank = "West"};
+	Room* ShackLocs[4] = {Village};
+	Room* BankLocs[4] = {Villagecenter;
+	/////////////////////////////////////////////////////////////////////
+	Room* VillageCent = new Room("Village Center",VillageCentlocs,"In the center of the village there is a small well. Next to the well is an old man with a long beard and a long staff. He appears to be very wise and very lost. To the south of the center is the small shack from where the player came from. To the East is a fenced graveyard. To the West is a bank. To the North is a large house.");
+	Room* Shack = new Room("Shack",ShackLocs,"You wake up in a shack. A little girl that tells you how she dragged you from outside. She tells you that there was a recent fire in the village and that she managed to save you. In this room there is an apple, a butter knife, and a journal. The door leads to outside.");
+
+
+}
+
+/*
+Village Center------------------------------------------------------------------
+In the center of the village there is a small well. Next to the well is an old man with a long beard and a long staff. He
+appears to be very wise and very lost. To the south of the center is the small shack from where the player came from. To the
+East is a fenced graveyard. To the West is a bank. To the North is a large house.
+
+Shack
+The player will wake up in a room. In the room is a little girl that tells the player how she dragged him from the outside.
+She tells him that there was a recent fire in the village and that she managed to save him. In this room there is an apple,
+a butter knife, and a journal. The door leads to outside.
+
+Bank----------------------------------------------------------------------------------------
+The Bank seemed to be underconstruction due to some unusual damages. You can't go into the bank because the people constructing
+the bank will not let you in. Once you give them the gold you found in the Graveyard, then they will allow you to enter. In the bank
+there is a lot of stuff that was burnt from the fire. Weirdly enough, there is a wall that seemed to not be affected by the fire at all.
+You do something to activate the wall. 
